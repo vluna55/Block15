d86f7c7eadb72dc7515ebb42f8f06b3959a857c9
LuiguiGallardo <bfllg77@gmail.com>
2023-10-05 06:22:10 -0700

    Code update using a function

--- index.js
+++ index.js
@@ -1,24 +1,35 @@
-// User input
-const input = prompt(
-    "Input the Flavors you want!"
-  );
 
-// Split the input into an array of flavors
-const flavors = input.split(',');
+// Function definition
+function countFlavors(input) {
+  // Split the input into an array of flavors
 
-// Create an object to store the flavor counts
-const flavorCount = {};
+  const flavors = input.split(',');
 
-// Create the flavor count
-for (const flavor of flavors) {
-  // Check if the flavor is present, if not add it to the object
-  if (!flavorCount[flavor]) {
-    flavorCount[flavor] = 0;
+  // Create an object to store the flavor counts
+  const flavorCount = {};
+
+  // Create the flavor count
+  for (const flavor of flavors) {
+    // Check if the flavor is present, if not add it to the object
+    if (!flavorCount[flavor]) {
+      flavorCount[flavor] = 0;
+    }
+
+    // Increment the count for the selected flavor
+    flavorCount[flavor]++;
   }
 
-  // Increment the count for the selected flavor
-  flavorCount[flavor]++;
+  // Display the table to the console with console.log
+  console.log('Flavor\tCount')
+  for (const flavor in flavorCount) {
+    console.log(`${flavor}\t${flavorCount[flavor]}`);
+  }
 }
 
-// Display the table to the console with console.table
-console.table(flavorCount)
+// User input
+const input = prompt(
+  "Input the Flavors you want!"
+);
+
+// Call the function
+countFlavors(input);
