d543f9754771782206f854bd817d01ba5fbf3045
Zakair <rossmopar33@gmail.com>
2023-10-05 15:45:22 -0600

    new update

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-let flavor = prompt(
+let iceCream = prompt(
     "Input the Flavors you want!"
   );
 
-const flavors = {
-    flavors:
-    ['vanilla', 'strawberry', 'coffee']
+const flavors = iceCream.split(',');
 
-} 
+const flavor_aaray = [flavors];
+let flavor_results = {};
+flavors.forEach((x) => {
+  flavor_results[x] = flavor_results[x] || 0;
+  flavor_results[x]++
+});
+console.log (flavor_aaray);
+window.alert('Are these the correct flavors ' + flavor_aaray);
