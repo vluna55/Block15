d47b4e45076cdaa1ae5320073196b6279cff52cd
LuiguiGallardo <bfllg77@gmail.com>
2023-10-05 06:06:14 -0700

    Solution to the workshop created.

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Block15
+## Workshop: Froyo Orders
+
+In this project, we will be building out the functionality of a simple app that asks a user a list of comma-separated froyo flavors and then output a table to the console according to the user's input.
+
+## Instructions
+
+1. Download the complete repository.
+2. Open the `index.html` using the browser.
+3. Input a comma-separated froyo flavors (e.g. `vanilla,vanilla,vanilla,strawberry,coffee,coffee`).
+4. Open the browser's console to see the output table.
--- index.js
+++ index.js
@@ -1,9 +1,24 @@
-let flavor = prompt(
+// User input
+const input = prompt(
     "Input the Flavors you want!"
   );
 
-const flavors = {
-    flavors:
-    ['vanilla', 'strawberry', 'coffee']
+// Split the input into an array of flavors
+const flavors = input.split(',');
 
-} 
+// Create an object to store the flavor counts
+const flavorCount = {};
+
+// Create the flavor count
+for (const flavor of flavors) {
+  // Check if the flavor is present, if not add it to the object
+  if (!flavorCount[flavor]) {
+    flavorCount[flavor] = 0;
+  }
+
+  // Increment the count for the selected flavor
+  flavorCount[flavor]++;
+}
+
+// Display the table to the console with console.table
+console.table(flavorCount)
